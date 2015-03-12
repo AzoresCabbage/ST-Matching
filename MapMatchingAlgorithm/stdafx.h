7aa1039d8b741a654d76e6b1eb7ae7c2194076e9
WangYujie <coolwyj@126.com>
2015-03-12 16:56:20 +0800

    update project file

---
+++ MapMatchingAlgorithm/stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <queue>
+#include <algorithm>
+#include <mutex>
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <map>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include "geometry.h"
+#include "database.h"
+#include "Graph.h"
+#include "libpq-fe.h"
+
+// TODO: 在此处引用程序需要的其他头文件
